4a9e3abb542266fe73efafaa58b2dfa1a4205efc
Gordon Weakliem <gordon@vrbo.com>
2016-03-02 20:41:13 -0700

    fixed jquery import

--- app/mock-sessions.ts
+++ app/mock-sessions.ts
@@ -9,6 +9,8 @@ export var SESSIONS: Session[] = [
   { "id": 13, "title":"The Next Step to Creating a Trust Organization",
     "date":"2016-02-29", "timeslot": "11:00-12:00", "space": "Rumors",
     "convener":"Bruce" },
+  { "id": 16, "title":"Can I respect myself after writing Ruby?", "convener":"Chris Phelps",
+    "date":"2016-02-29","timeslot": "11:00-12:00", "space": "Piano"},
   { "id": 14, "title":"Seeking failure as a counter-philosophy to demanding perfection",
     "date":"2016-03-01", "timeslot": "8:30-9:30", "space": "Stained Glass",
     "convener":"Bruce"  },
